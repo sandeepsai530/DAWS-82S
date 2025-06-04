1be20e2af183942537716e8c0ae9e5ee71cc70a4
sandeepsai530 <sandeepsai530@gmail.com>
2025-06-05 00:55:00 +0530

    added 05 file

--- shell-script/05-variables.sh
+++ shell-script/05-variables.sh
@@ -5,3 +5,6 @@ echo "please enter username:"
 #read USERNAME #username will be displayed on console while writing
 read -s USERNAME #by writing -s , username will not be displayed while writing on console
 echo "you entered username as : $USERNAME"
+
+echo "please enter password:"
+read -s PASSWORD
