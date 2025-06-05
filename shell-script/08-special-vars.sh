f59fc244221eab0b860ca86227c554bc01ad11ec
sandeepsai530 <sandeepsai530@gmail.com>
2025-06-05 18:39:19 +0530

    added files

--- shell-script/08-special-vars.sh
+++ shell-script/08-special-vars.sh
@@ -7,5 +7,6 @@ echo "present working directory: $PWD"
 echo "Home directory of current user: $HOME"
 echo "which user is running this script: $USER" 
 echo "process id of current script: $$"
+echo "previous command exit code status:$?"
 sleep 60 &
 echo "process id of last command running in background: $!"
