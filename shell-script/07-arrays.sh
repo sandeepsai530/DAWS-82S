daa886ca96f7ef46a7faf05f72a19a0bded4d690
sandeepsai530 <sandeepsai530@gmail.com>
2025-06-05 11:32:42 +0530

    added files

--- shell-script/07-arrays.sh
+++ shell-script/07-arrays.sh
@@ -2,8 +2,8 @@
 
 MOVIES=("Apple" "Beetroot" "Carrot")
 
-echo "First movie name: ${MOVIES[$0]}"
-echo "Second movie name: ${MOVIES[$1]}"
-echo "Third movie name: ${MOVIES[$2]}"
+echo "First movie name: ${MOVIES[0]}"
+echo "Second movie name: ${MOVIES[1]}"
+echo "Third movie name: ${MOVIES[2]}"
 
-echo "To print all movies use @: ${MOVIES[$@]}"
+echo "To print all movies use @: ${MOVIES[@]}"
