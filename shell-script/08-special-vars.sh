71e2b6632bb904c1ed8ba7608d658a106a7004c6
sandeepsai530 <sandeepsai530@gmail.com>
2025-06-05 21:06:06 +0530

    added files

--- shell-script/08-special-vars.sh
+++ shell-script/08-special-vars.sh
@@ -3,10 +3,10 @@
 echo "All variables passed: $@"
 echo "number of varibales passed: $#"
 echo "script name: $0"
+sleep 60 &
+echo "process id of last command running in background: $!"
+echo "process id of current script: $$"
+echo "previous command exit code status:$?"
 echo "present working directory: $PWD"
 echo "Home directory of current user: $HOME"
 echo "which user is running this script: $USER" 
-echo "process id of current script: $$"
-echo "previous command exit code status:$?"
-sleep 60 &
-echo "process id of last command running in background: $!"
--- shell-script/10-install-script.sh
+++ shell-script/10-install-script.sh
@@ -8,3 +8,17 @@ then
     exit 1; #other than 0
 fi
 dnf list installed mysql
+
+if [ $? -ne 0 ]
+then #not installed
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing MYSQL...FAILURE"
+        exit 1
+    else
+        echo "Installing MYSQL...SUCCESS"
+    fi
+else
+    echo "MYSQL is already ..INSTALLED"
+fi
