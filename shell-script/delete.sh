a80676a5ea4513d613ff96295259c7a11c2d1642
sandeepsai530 <sandeepsai530@gmail.com>
2025-06-09 15:49:32 +0530

    added files

--- shell-script/delete.sh
+++ shell-script/delete.sh
@@ -8,12 +8,23 @@ then
     exit 1
 fi
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
 for package in $@
 do
     dnf list installed $package
     if [ $? -ne 0 ]
     then
         dnf install $package -y
+        VALIDATE $? "installing $package"
     else
         echo "package already installed"
     fi
