1910b60baa4dc3333dc4ea96fc4f3898a7cb789c
sandeepsai530 <sandeepsai530@gmail.com>
2025-06-09 19:35:53 +0530

    added files

---
+++ expense-shell/frontend.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then   
+        echo -e "$2...$R FAILURE"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS"
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR: you must have root access to execute this script"
+        exit 1; #other than 0
+    fi
+}
+
+echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+
+dnf install nginx -y
+VALIDATE $? "Installing nginx server"
+
+systemctl enable nginx
+VALIDATE $? "Enabling nginx"
+
+systemctl start nginx
+VALIDATE $? "starting nginx"
+
+rm -rf /usr/share/nginx/html/*
+VALIDATE $? "removing old content"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+VALIDATE $? "Downloading frontend code"
+
+cd /usr/share/nginx/html
+VALIDATE $? "extracting the code"
+
+unzip /tmp/frontend.zip
+VALIDATE $? "unzip the frontend code"
+
+systemctl restart nginx
+VALIDATE $? "restarting nginx"
