31f904db816eafcbcc666c836d65531012a5a8a6
sandeepsai530 <sandeepsai530@gmail.com>
2025-06-09 13:08:03 +0530

    added files

--- shell-script/delete.sh
+++ shell-script/delete.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 USERID=$(id -u)
-package=$@
-echo "$package"
+package=$1
 
 if [ $USERID -ne 0 ]
 then   
     echo "please login using root access"
     exit 1
 fi
-
-
+echo "$package"
+if [ $? -ne 0 ]
+then
+    echo "please enter software names"
+    exit 1
+fi
