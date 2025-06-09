02eb01e31e681dded6cc4504dd48b5d8e4b072c1
sandeepsai530 <sandeepsai530@gmail.com>
2025-06-09 11:38:02 +0530

    added files

---
+++ shell-script/13-logs.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+echo "$LOG_FILE_NAME"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then   
+        echo -e "$2...$R FAILURE"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR: you must have root access to execute this script"
+    exit 1; #other than 0
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then #not installed
+    dnf install mysql -y
+    VALIDATE $? "Installing MySQL"
+else
+    echo -e "MySQL is already ... $Y INSTALLED"
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then #not installed
+    dnf install git -y
+    VALIDATE $? "Installing GIT"
+else   
+    echo -e "Git is already .. $Y INSTALLED"
+fi
+
