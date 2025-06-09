c9e79c6458b22761c8288ceca9a290e7d25c99d0
sandeepsai530 <sandeepsai530@gmail.com>
2025-06-09 17:50:19 +0530

    added files

--- expense-shell/mysql.sh
+++ expense-shell/mysql.sh
@@ -42,7 +42,7 @@ VALIDATE $? "Enabling MySQL server"
 systemctl start mysqld &>>$LOG_FILE_NAME
 VALIDATE $? "Starting MySQL server"
 
-mysql -h saisandeep-devops.xyz -u root -pExpenseApp@1 -e 'show databases'
+mysql -h mysql.saisandeep-devops.xyz -u root -pExpenseApp@1 -e 'show databases;'
 
 if [ $? -ne 0 ]
 then   
