7ba5787470164370b0fd541768913fb14a46cddd
sandeepsai530 <sandeepsai530@gmail.com>
2025-06-09 12:08:49 +0530

    added files

--- shell-script/13-logs.sh
+++ shell-script/13-logs.sh
@@ -11,7 +11,7 @@ LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-echo "$LOG_FILE_NAME"
+echo "script started executing at: $TIMESTAMP"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
